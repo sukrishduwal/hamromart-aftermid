2e53c3d77297e868c5802de472e6acc437c51263
sukrishduwal <sukrishduwal17@gmail.com>
2026-07-12 15:14:49 +0545

    Discount

--- sales/admin.py
+++ sales/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Sale, SaleItem
+from .models import DiscountScheme
 
 class SaleAdmin(admin.ModelAdmin):
     change_list_template = 'admin/sale_changelist.html'
 
 admin.site.register(Sale, SaleAdmin)
 admin.site.register(SaleItem)
+@admin.register(DiscountScheme)
+class DiscountSchemeAdmin(admin.ModelAdmin):
+    list_display = (
+        'min_purchase',
+        'step_amount',
+        'step_discount',
+        'max_discount',
+        'is_active'
+    )
